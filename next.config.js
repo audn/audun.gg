bd3fede0939a0bcc900d1a3f83b195f43837d586
audn <audunhilden@live.no>
2021-05-24 09:50:17 +0200

    feat: discord redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/discord',
+        destination: 'https://discord.gg/ha2hVp8KV5',
+      },
+    ];
+  },
   images: {
     domains: ['images.unsplash.com'],
   },
